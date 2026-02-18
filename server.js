2a173a7ed907ed475939fef909830553a638ef4a
Cristiano_Modder <81588557+cristianomodder@users.noreply.github.com>
2026-02-18 00:22:22 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,25 +3,38 @@ import express from "express";
 const app = express();
 
 app.get("/testar", async (req, res) => {
+  try {
 
-  const response = await fetch("https://httpbin.org/post", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      mensagem: "POST saindo do Render cristiano"
-    })
-  });
+    const payload = {
+      id: "5a40754d-5a2f-4345-b317-71e28553a408",
+      external_id: "A7ECIQJ1BU",
+      total_amount: "525",
+      status: "AUTHORIZED",
+      payment_method: "PIX"
+    };
 
-  const data = await response.json();
+    const response = await fetch("https://gtex.beefund.co/ipn/pix", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    });
 
-  res.json({
-    sucesso: true,
-    destino: data
-  });
+    const text = await response.text();
+
+    res.json({
+      sucesso: true,
+      resposta_destino: text
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      erro: error.message
+    });
+  }
 });
 
 app.listen(10000, () => {
-  console.log("Servidor rodando");
+  console.log("Servidor rodando na porta 10000");
 });
