b8de00f4bc14412a723d285ece43561f3b6dc2f4
Cristiano_Modder <81588557+cristianomodder@users.noreply.github.com>
2026-02-24 14:43:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,22 +3,35 @@ import express from "express";
 const app = express();
 
 app.get("/testar", async (req, res) => {
+  try {
+    const payload = {
+      external_id: "UQSJRWJ12F",
+      status: "AUTHORIZED",
+      timestamp: "2026-02-24 14:39:33"
+    };
 
-  const response = await fetch("https://httpbin.org/post", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      mensagem: "POST saindo do Render"
-    })
-  });
+    const response = await fetch("https://beefund.co/ipn/pix", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    });
 
-  const data = await response.json();
+    const data = await response.text();
 
-  res.json(data);
+    res.json({
+      enviado: payload,
+      resposta: data
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      erro: error.message
+    });
+  }
 });
 
 app.listen(10000, () => {
-  console.log("Servidor rodando");
+  console.log("Servidor rodando na porta 10000");
 });
